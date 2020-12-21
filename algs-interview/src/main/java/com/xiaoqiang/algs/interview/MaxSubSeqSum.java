867549cc2a90d028577f0e047c044ee129c2d7ab
小强 <756164814@qq.com>
2020-12-22 01:14:14 +0800

    剑指 Offer 42. 连续子数组的最大和

--- algs-interview/src/main/java/com/xiaoqiang/algs/interview/MaxSubSeqSum.java
+++ algs-interview/src/main/java/com/xiaoqiang/algs/interview/MaxSubSeqSum.java
@@ -1,44 +1,28 @@
 package com.xiaoqiang.algs.interview;
 
 /**
- * 面试题: 求给定一个数组中，求最大连续子数组之和
- * 例如：[1,3,-1,2,-4,5], 最大连续子数组[1,3,-1,2,-4,5],结果为6
- *      [-2,1,-3,4,-1,2,1,-5,4], 最大连续子数组[4,-1,2,1],结果为6
- * 思路：
- *     r1 = max(n1)
- *     r2 = max(n1,n2)
- *     r3 = max(n1,n2,n3, n1+n2, n2+n3)
- *     ri = max(n1...i,n1+n2,...ni-1+ni, ... n1+n2+...ni)
- *     r = [
- *     [n1,n2,n3,...ni],
- *     [n1+n2,n2+n3,...ni-1+ni],
- *     [n1+n2+n3,n2+n3+n4, ni-2 + ni-1 +ni],
- *     ...
- *     ]
- * 归纳：
- *     r[i][j] = r[i-1][j]+r[0][j-i]
+ * 输入: nums = [-2,1,-3,4,-1,2,1,-5,4]
+ * 输出: 6
+ * 解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
+ * 
+ * 最优解: dp[i] = dp[i-1]>0?dp[i-1]+nums[i]:nums[i]
  */
 public class MaxSubSeqSum {
     public static void main(String[] args) {
-        System.out.println(maxSubSeqSum(new int[]{1, 3, -1, 2, -4, 5}));
-        System.out.println(maxSubSeqSum(new int[]{-2,1,-3,4,-1,2,1,-5,4}));
+        System.out.println(maxSubArray(new int[]{1, 3, -1, 2, -4, 5}));
+        System.out.println(maxSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
     }
 
-    public static int maxSubSeqSum(int[] arr){
-        int[][]r = new int[arr.length][];
-        int max = Integer.MIN_VALUE;
-        r[0] = new int[arr.length];
-        for (int i = 0; i < arr.length; i++) {
-            r[0][i] = arr[i];
-            max = Math.max(arr[i], max);
-        }
-        for (int i = 1; i < arr.length; i++) {
-            r[i] = new int[arr.length-i];
-            for (int j = 0; j < arr.length - i; j++) {
-                r[i][j] = r[i-1][j]+r[0][j+i];
-                max = Math.max(r[i][j], max);
+    public static int maxSubArray(int[] nums) {
+        int max = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i-1] > 0) {
+                nums[i] = nums[i - 1] + nums[i];
             }
+            max = Math.max(nums[i], max);
         }
         return max;
     }
+
+
 }
