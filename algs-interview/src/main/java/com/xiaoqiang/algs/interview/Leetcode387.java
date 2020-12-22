84dca48c4d2e92ce0dd778802ff8663007b58fea
小强 <756164814@qq.com>
2020-12-23 01:20:53 +0800

    leetcode: 387. 字符串中的第一个唯一字符

---
+++ algs-interview/src/main/java/com/xiaoqiang/algs/interview/Leetcode387.java
@@ -0,0 +1,30 @@
+package com.xiaoqiang.algs.interview;
+
+/**
+ * 387. 字符串中的第一个唯一字符
+ *     charTable[i][2],分别存储某一个字符出现的次数和位置, i = c - 'a'
+ */
+public class Leetcode387 {
+    public static void main(String[] args) {
+        System.out.println(new Leetcode387().firstUniqChar("itwqbtcdprfsuprkrjkausiterybzncbmdvkgljxuekizvaivszowqtmrttiihervpncztuoljftlxybpgwnjb"));
+    }
+
+    public int firstUniqChar(String s) {
+        char[] chars = s.toCharArray();
+        int[][] charTable = new int['z' - 'a' + 1][2];
+        for (int i = 0; i < chars.length; i++) {
+            if (charTable[chars[i] - 'a'][0]>1) {
+                continue;
+            }
+            charTable[chars[i] - 'a'][0] += 1;
+            charTable[chars[i] - 'a'][1] = i;
+        }
+        int firsIndex = chars.length+1;
+        for (int[] charCountInfo : charTable) {
+            if (charCountInfo[0] == 1) {
+                firsIndex = Math.min(charCountInfo[1], firsIndex);
+            }
+        }
+        return firsIndex == chars.length+1 ? -1 : firsIndex;
+    }
+}
