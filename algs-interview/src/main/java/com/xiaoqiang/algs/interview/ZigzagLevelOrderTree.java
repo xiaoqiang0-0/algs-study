32dc49568563940038367846f2160741dfb49999
xiaoqiang <756164814@qq.com>
2020-12-22 10:48:21 +0800

    leetcode 103. 二叉树的锯齿形层序遍历  > 去除每层最后结果的转置操作，直接从插入时更具flag确认插入first或者last

--- algs-interview/src/main/java/com/xiaoqiang/algs/interview/ZigzagLevelOrderTree.java
+++ algs-interview/src/main/java/com/xiaoqiang/algs/interview/ZigzagLevelOrderTree.java
@@ -4,14 +4,14 @@ import java.util.*;
 
 /**
  * 给定一个二叉树，返回其节点值的锯齿形层序遍历。（即先从左往右，再从右往左进行下一层遍历，以此类推，层与层之间交替进行）。
- *
+ * <p>
  * 例如：
  * 给定二叉树 [3,9,20,null,null,15,7],
- *
+ * <p>
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/binary-tree-zigzag-level-order-traversal
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
- *
+ * <p>
  * Definition for a binary tree node.
  * public class TreeNode {
  * int val;
@@ -32,18 +32,25 @@ public class ZigzagLevelOrderTree {
 
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         List<List<Integer>> result = new LinkedList<>();
+        if (root==null) {
+            return result;
+        }
         Queue<TreeNode> lq = new LinkedList<>();
         Queue<TreeNode> rq = new LinkedList<>();
         lq.offer(root);
         boolean flag = true;
-        List<Integer> curLevel = new LinkedList<>();
+        LinkedList<Integer> curLevel = new LinkedList<>();
         while (!lq.isEmpty() || !rq.isEmpty()) {
             Queue<TreeNode> cq = flag ? lq : rq;
             Queue<TreeNode> nq = flag ? rq : lq;
 
             TreeNode n = cq.poll();
             if (n != null) {
-                curLevel.add(n.val);
+                if (flag) {
+                    curLevel.addLast(n.val);
+                } else {
+                    curLevel.addFirst(n.val);
+                }
             }
             if (n != null && n.left != null) {
                 nq.offer(n.left);
@@ -53,12 +60,7 @@ public class ZigzagLevelOrderTree {
             }
 
             if (cq.isEmpty()) {
-                if (!flag) {
-                    Collections.reverse(curLevel);
-                }
-                if (!curLevel.isEmpty()) {
-                    result.add(curLevel);
-                }
+                result.add(curLevel);
                 curLevel = new LinkedList<>();
                 flag = !flag;
             }
