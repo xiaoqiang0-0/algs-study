8953e66e27a96bad984e3190ebbbdb71b9935b70
xiaoqiang <756164814@qq.com>
2020-12-23 14:58:42 +0800

    add commons module

---
+++ commons/src/main/java/com/xiaoqiang/algs/struct/LinkNode.java
@@ -0,0 +1,13 @@
+package com.xiaoqiang.algs.struct;
+
+public class LinkNode {
+    public int val;
+    public LinkNode next;
+
+    public LinkNode() {
+    }
+
+    public LinkNode(int val) {
+        this.val = val;
+    }
+}
---
+++ commons/src/main/java/com/xiaoqiang/algs/struct/TreeNode.java
@@ -0,0 +1,14 @@
+package com.xiaoqiang.algs.struct;
+
+public class TreeNode {
+    public int data;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode() {
+    }
+
+    public TreeNode(int data) {
+        this.data = data;
+    }
+}
