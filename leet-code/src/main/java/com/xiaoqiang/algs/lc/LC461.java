66218656e9389741766f937f2c95c5611c5b2c94
小强 <756164814@qq.com>
2021-05-28 09:42:07 +0800

    LeetCode 总汉明距离

--- leet-code/src/main/java/com/xiaoqiang/algs/lc/LC461.java
+++ leet-code/src/main/java/com/xiaoqiang/algs/lc/LC461.java
@@ -3,6 +3,7 @@ package com.xiaoqiang.algs.lc;
 public class LC461 {
     public static void main(String[] args) {
         System.out.println(new Solution().hammingDistance(1, 6));
+        System.out.println(new Solution().totalHammingDistance(new int[]{4, 14, 2}));
     }
 }
 
@@ -10,6 +11,18 @@ class Solution {
 
     public int lowbit(int x) {
         return x & -x;
+    }
+
+    public int totalHammingDistance(int[] nums) {
+        int distance = 0;
+        for (int i = 0; i < 3; i++) {
+            int cnt = 0;
+            for (int j = 0; j < nums.length; j++) {
+                cnt += nums[j] >> j & 1;
+            }
+            distance += cnt * (nums.length - cnt);
+        }
+        return distance;
     }
 
     public int hammingDistance(int x, int y) {
