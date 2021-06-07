187d911f624eb6dbecf9634bdae2509a7101e46b
小强 <756164814@qq.com>
2021-06-07 15:15:51 +0800

    二分查找第一个大于目标值的下标，如果不存在则返回最大下标+1

--- algs-interview/src/main/java/com/xiaoqiang/algs/interview/BinSearch.java
+++ algs-interview/src/main/java/com/xiaoqiang/algs/interview/BinSearch.java
@@ -3,7 +3,10 @@ package com.xiaoqiang.algs.interview;
 public class BinSearch {
     public static void main(String[] args) {
         System.out.println(new BinSearch().search(new int[]{-1, 0, 5}, 5));
-
+        System.out.println(new BinSearch().binSearchFirst(new int[]{1, 2, 3, 3, 5}, 3));
+        System.out.println(new BinSearch().binSearchFirst(new int[]{1, 2, 3, 3, 5}, 4));
+        System.out.println(new BinSearch().binSearchFirst(new int[]{1, 2, 3, 3, 5}, 5));
+        System.out.println(new BinSearch().binSearchFirst(new int[]{1, 2, 3, 3, 5}, 6));
     }
 
     public int search(int[] nums, int target) {
@@ -38,6 +41,32 @@ public class BinSearch {
                 l = m + 1;
             }
         }
+        return pos;
+    }
+
+    public int binSearchFirst(int[] nums, int target) {
+        int pos = nums.length;
+
+        int low = 0;
+        int high = pos - 1;
+        while (high >= low) {
+            if (high - low < 2) {
+                if (nums[low] >= target) {
+                    pos = low;
+                } else if (nums[high] >= target) {
+                    pos = high;
+                }
+                break;
+            }
+            int m = (low + high) / 2;
+            if (nums[m] >= target) {
+                pos = m;
+                high = m - 1;
+            } else {
+                low = m + 1;
+            }
+        }
+
         return pos;
     }
 
